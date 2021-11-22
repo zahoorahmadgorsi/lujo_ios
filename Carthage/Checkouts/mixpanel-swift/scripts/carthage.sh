d353a8a042ec2ac3f48629045cff2888384eac15
Zahoor Ahmad Gorsi <zahoor.gorsi@gmail.com>
2021-11-22 21:21:15 +0500

    Delete Carthage directory
    hi, no need to keep Carthage folder

--- Carthage/Checkouts/mixpanel-swift/scripts/carthage.sh
+++
@@ -1,3 +0,0 @@
-export XCODE_XCCONFIG_FILE=$PWD/scripts/carthage.xcconfig
-carthage build --no-skip-current
-carthage archive Mixpanel
--- Carthage/Checkouts/mixpanel-swift/scripts/generate_docs.sh
+++
@@ -1,8 +0,0 @@
-jazzy \
---clean \
--a Mixpanel \
--u http://mixpanel.com \
---github_url https://github.com/mixpanel/mixpanel-swift \
---module-version 2.10.1 \
---framework-root . \
---module Mixpanel
