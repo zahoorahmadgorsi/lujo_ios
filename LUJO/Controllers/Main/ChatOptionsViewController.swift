c8bfaf60d931a2ed1f8594988226b6e56436fc51
Zahoor <zahoor.gorsi@gmail.com>
2022-01-30 16:05:54 +0500

    Replacing chat files with conversation

--- LUJO/Controllers/Main/ChatOptionsViewController.swift
+++ LUJO/Controllers/Main/ChatOptionsViewController.swift
@@ -38,26 +38,110 @@ class ChatOptionsViewController: UIViewController {
     
     
     @IBAction func btnFindATableTapped(_ sender: Any) {
-        print("btnFindATableTapped")
+        let viewController = AdvanceChatViewController()
+        viewController.product = Product(id: -1 , type: "restaurant" , name: "Restaurant Inquiry")
+        let navController = UINavigationController(rootViewController:viewController)
+        if #available(iOS 13.0, *) {
+            let controller = navController.topViewController
+            // Modal Dismiss iOS 13 onward
+            //to call UIAdaptivePresentationControllerDelegate.presentationControllerDidDismiss at dismiss by pressing cross button
+            controller?.presentationController?.delegate = self
+        }
+        //to call UIAdaptivePresentationControllerDelegate.presentationControllerDidDismiss at dismiss by dragging
+        navController.presentationController?.delegate = self
+        UIApplication.topViewController()?.present(navController, animated: true, completion: nil)
     }
     
     @IBAction func btnEventTapped(_ sender: Any) {
-        print("btnEventTapped")
+        let viewController = AdvanceChatViewController()
+        viewController.product = Product(id: -1 , type: "event" , name: "Event Inquiry")
+        let navController = UINavigationController(rootViewController:viewController)
+        if #available(iOS 13.0, *) {
+            let controller = navController.topViewController
+            // Modal Dismiss iOS 13 onward
+            //to call UIAdaptivePresentationControllerDelegate.presentationControllerDidDismiss at dismiss by pressing cross button
+            controller?.presentationController?.delegate = self
+        }
+        //to call UIAdaptivePresentationControllerDelegate.presentationControllerDidDismiss at dismiss by dragging
+        navController.presentationController?.delegate = self
+        UIApplication.topViewController()?.present(navController, animated: true, completion: nil)
     }
     
     @IBAction func btnAviationTapped(_ sender: Any) {
-        print("btnAviationTapped")
+        let viewController = AdvanceChatViewController()
+        viewController.product = Product(id: -1 , type: "aviation" , name: "Aviation Inquiry")
+        let navController = UINavigationController(rootViewController:viewController)
+        if #available(iOS 13.0, *) {
+            let controller = navController.topViewController
+            // Modal Dismiss iOS 13 onward
+            //to call UIAdaptivePresentationControllerDelegate.presentationControllerDidDismiss at dismiss by pressing cross button
+            controller?.presentationController?.delegate = self
+        }
+        //to call UIAdaptivePresentationControllerDelegate.presentationControllerDidDismiss at dismiss by dragging
+        navController.presentationController?.delegate = self
+        UIApplication.topViewController()?.present(navController, animated: true, completion: nil)
     }
     
     @IBAction func btnCharterAYachtTapped(_ sender: Any) {
-        print("btnCharterAYachtTapped")
+        let viewController = AdvanceChatViewController()
+        viewController.product = Product(id: -1 , type: "yacht" , name: "Yacht Inquiry")
+        let navController = UINavigationController(rootViewController:viewController)
+        if #available(iOS 13.0, *) {
+            let controller = navController.topViewController
+            // Modal Dismiss iOS 13 onward
+            //to call UIAdaptivePresentationControllerDelegate.presentationControllerDidDismiss at dismiss by pressing cross button
+            controller?.presentationController?.delegate = self
+        }
+        //to call UIAdaptivePresentationControllerDelegate.presentationControllerDidDismiss at dismiss by dragging
+        navController.presentationController?.delegate = self
+        UIApplication.topViewController()?.present(navController, animated: true, completion: nil)
     }
     
     @IBAction func btnVillaTapped(_ sender: Any) {
-        print("btnVillaTapped")
+        let viewController = AdvanceChatViewController()
+        viewController.product = Product(id: -1 , type: "villa" , name: "Villa Inquiry")
+        let navController = UINavigationController(rootViewController:viewController)
+        if #available(iOS 13.0, *) {
+            let controller = navController.topViewController
+            // Modal Dismiss iOS 13 onward
+            //to call UIAdaptivePresentationControllerDelegate.presentationControllerDidDismiss at dismiss by pressing cross button
+            controller?.presentationController?.delegate = self
+        }
+        //to call UIAdaptivePresentationControllerDelegate.presentationControllerDidDismiss at dismiss by dragging
+        navController.presentationController?.delegate = self
+        UIApplication.topViewController()?.present(navController, animated: true, completion: nil)
     }
     
     @IBAction func btnTravelTapped(_ sender: Any) {
-        print("btnTravelTapped")
+        let viewController = AdvanceChatViewController()
+        viewController.product = Product(id: -1 , type: "travel" , name: "Hotel Inquiry")
+        let navController = UINavigationController(rootViewController:viewController)
+        if #available(iOS 13.0, *) {
+            let controller = navController.topViewController
+            // Modal Dismiss iOS 13 onward
+            //to call UIAdaptivePresentationControllerDelegate.presentationControllerDidDismiss at dismiss by pressing cross button
+            controller?.presentationController?.delegate = self
+        }
+        //to call UIAdaptivePresentationControllerDelegate.presentationControllerDidDismiss at dismiss by dragging
+        navController.presentationController?.delegate = self
+        UIApplication.topViewController()?.present(navController, animated: true, completion: nil)
+    }
+}
+
+extension ChatOptionsViewController: UIAdaptivePresentationControllerDelegate {
+    // Only called when the sheet is dismissed by DRAGGING as well as when tapped on cross button
+    public func presentationControllerDidDismiss( _ presentationController: UIPresentationController) {
+        if #available(iOS 13, *) {
+            //Call viewWillAppear only in iOS 13
+            //so that receivedNewMessage should stop calling on AdvanceChatViewController and start calling on homeViewcontroller
+            
+            if let tabbarController = self.presentingViewController as? MainTabBarController{
+                if let navViewController = tabbarController.viewControllers?[0] as? UINavigationController {
+                    if let viewController = navViewController.children.first as?  HomeViewController{
+                        ConversationsManager.sharedConversationsManager.delegate = viewController
+                    }
+                }
+            }
+        }
     }
 }
